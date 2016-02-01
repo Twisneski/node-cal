d0493fcf815230833849c9ba6f360fecf4fcff44
Twisneski <tina.wisneski525@gmail.com>
2016-02-01 09:47:03 -0600

    added babel and renamed cal to cli and

--- cal.js
+++ cal.js
@@ -1,38 +1,7 @@
-#!/usr/bin/env node --harmony_destructuring
+#!/usr/bin/env node
 
 'use strict';
 
-console.log('cal');
-
-
-const generateMonth = require('./lib/month');
-
-//console.log(month(2016,1));
-
-//console.log(process.argv)
-
-//const [,, ...arg] = proces.argv;
-
-//if (args.length === 2) {
-  //const [month, year] = args;
-
-//console.log('generateMonth(${year},${month}');
-//} else if (args.length === 1) {
-  //const [year] = arg;
-
-  //console.log('generateYear(${year})')
-//} else {
-  //console.log('dun broke it');
-  //process.exit(64);
-//}
-
-
-
-
-
-
-
-
-
-
+require('babel-register');
+require('./lib/cli');
 
