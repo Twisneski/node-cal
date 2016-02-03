692648a046e4c3f1df3cb53a99466e5c35a0ee60
Twisneski <tina.wisneski525@gmail.com>
2016-02-03 06:48:17 -0600

    cleaned up code

--- cal.js
+++ cal.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 'use strict';
-
 require('babel-register');
 require('./lib/cli');
+
 
--- tests/test.js
+++ tests/test.js
@@ -1,233 +1,105 @@
 'use strict';
 
-//describe('test suite', () => {
-  //it('true should be true', () => {
-    //expect(true).to.be.true;
-  //});
-//});
-
-
-
 const { expect } = require('chai');
 const { execSync } = require('child_process');
 
 describe('cal', () => {
   describe('CLI', () => {
-    it('should handle the current month', () => {
-     const goal = execSync('cal').toString();
-     const output = execSync('./cal.js').toString();
+   it('should handle the current month', () => {
+      const goal = execSync('cal').toString();
+      const output = execSync('./cal.js').toString();
       expect(output).to.equal(goal);
-    });
+   });
+   it('CLI should show Feb 2015 as having 4 weeks starts on Su', () => {
+      expect(execSync('./cal.js 2 2015').toString()).to.equal(execSync('cal 2 2015').toString());
+   });
+   it('CLI should show Oct 2015 as having 5 weeks starts on Th', () => {
+      expect(execSync('./cal.js 10 2015').toString()).to.equal(execSync('cal 10 2015').toString());
+   });
+   it('CLI should show Aug 2015 as having 6 weeks starts on Sa', () => {
+      expect(execSync('./cal.js 8 2015').toString()).to.equal(execSync('cal 8 2015').toString());
+   });
+   it('CLI should show Nov 2015 as having 30 days starts on Su', () => {
+      expect(execSync('./cal.js 11 2015').toString()).to.equal(execSync('cal 11 2015').toString());
+   });
+   it('CLI should show Dec 2015 as having 31 days starts on Tu', () => {
+      expect(execSync('./cal.js 12 2015').toString()).to.equal(execSync('cal 12 2015').toString());
+   });
+   it('CLI should show Feb 2014 as having 28 days starts on Sa', () => {
+      expect(execSync('./cal.js 2 2014').toString()).to.equal(execSync('cal 2 2014').toString());
+   });
+    it('CLI should show Feb 2012 as having 29 days starts on We', () => {
+      expect(execSync('./cal.js 2 2012').toString()).to.equal(execSync('cal 2 2012').toString());
+   });
+   it('CLI should show 2016', () => {
+      expect(execSync('./cal.js 2016').toString()).to.equal(execSync('cal 2016').toString());
+   });
+
   });
 
   describe("Zeller's congruence", () => {
-    const zellers = require('../lib/zeller.js');
-
+    var zellers = require('../lib/zellers.js');
     describe('.modifiedMonth', () => {
-      const goal = execSync('cal').toString();
-      const output = execSync('./cal.js 2016 1').toString();
       it('return 13 for January', () => {
         expect(zellers.modifiedMonth(1)).to.equal(13);
       });
-    });
-    describe('.modifiedMonth', () => {
-     const goal = execSync('cal').toString();
-     const output = execSync('./cal.js 2016 2').toString();
-      it('return 14 for February', () => {
+       it('return 14 for February', () => {
         expect(zellers.modifiedMonth(2)).to.equal(14);
-      // 2 === 14
-       });
-     });
-     describe('.modifiedMonth', () => {
-       const goal = execSync('cal').toString();
-       const output = execSync('./cal.js 2016 3').toString();
-      it('return 3 for March', () => {
+      });
+       it('return 3 for March', () => {
         expect(zellers.modifiedMonth(3)).to.equal(3);
-      // 3 === 3
-     });
-     });
-
-
+      });
+    });
 
     describe('.modifiedYear', () => {
-      const goal = execSync('cal').toString();
-      const output = execSync('./cal.js 2015 1').toString();
-      it('returns 2014 for Jan 2015', () => {
+      it('returns 2015 for Jan 2015', () => {
         expect(zellers.modifiedYear(2015, 1)).to.equal(2014);
-        });
       });
-
-    describe('.modifiedYear', () => {
-      const goal = execSync('cal').toString();
-      const output = execSync('./cal.js 2016 2').toString();
-      it('returns 2015 for Feb 2015', () => {
-        expect(zellers.modifiedYear(2016, 2)).to.equal(2015);
-       });
+       it('returns 2015 for Feb 2015', () => {
+        expect(zellers.modifiedYear(2016, 1)).to.equal(2015);
       });
-        // 2016, 2 === 2015
-
-     describe('.modifiedYear', () => {
-      const goal = execSync('cal').toString();
-      const output = execSync('./cal.js 2017 3').toString();
        it('returns 2017 for March 2017', () => {
         expect(zellers.modifiedYear(2017, 3)).to.equal(2017);
-         });
-            });
-         // 2017, 3 === 2017
-
+      });
+    });
 
     describe('.getDay', () => {
-      const goal = execSync('cal').toString();
-      const output = execSync('./cal.js 2016 3').toString();
       it('returns 2 (Tuesday) for March 1, 2016', () => {
-        expect(zellers.getDay(2016, 3, 1)).to.equal(2);
+        expect(zellers.modDay(2016, 3, 1)).to.equal(2);
       });
-     });
-    describe('.getDay', () => {
-      const goal = execSync('cal').toString();
-      const output = execSync('./cal.js 2000 3').toString();
-      it('returns 3 (Wed) for March 1, 2000', () => {
-        expect(zellers.getDay(2000, 3, 1)).to.equal(3);
-       });
+      it('returns 3 (Wednesday) for March 1, 2000', () => {
+        expect(zellers.modDay(2000, 3, 1)).to.equal(3);
       });
-      // 2000, 3, 1 === 3
-     describe('.getDay', () => {
-      it('returns 1 (Mon) for March 2, 2200', () => {
-        expect(zellers.getDay(2100, 3, 1)).to.equal(1);
-         });
-       });
-      // 2100, 3, 1 === 1 day of the week
-     describe('.getDay', () => {
-      it('returns 0 (Sun) for March 2, 2200', () => {
-        expect(zellers.getDay(2200, 3, 2)).to.equal(0);
-         });
-       });
-      // 2200, 3, 2 === 0
-     describe('.getDay', () => {
-      it('returns 4 (Thur) for March 2, 2200', () => {
-        expect(zellers.getDay(2300, 3, 1)).to.equal(4);
-         });
-       });
-         // 2300, 3, 1 === 4
-
-//NEED TO WRITE CODE ON THESE TESTS
-
-       //describe('.center', () => {
-        //it('should handle January', () => {
-       //expect(center('January 2016', () => {
-         //});
-        //});
-        //it('should handle February 2016', () => {
-          //expect(center('February 2016', () =>{
-          //});
-        //});
-      //});
-        //this is to test centering of the month and year
-
-        describe('.getSixWeek', () => {
-           const month = require('../lib/month.js');
-          it('return 6 week month', () => {
-            expect(month.getSixWeek(2016, 1)).to.equal(6);
-          });
-           it('return 6 week month', () => {
-            expect(month.getSixWeek(2016, 10)).to.equal(6);
-          });
-          it('return 6 week month', () => {
-            expect(month.getSixWeek(2016, 10)).to.equal(4);
-          });
-          it('return 6 week month', () => {
-            expect(month.getSixWeek(2016, 1)).to.equal(4);
-          });
-        });
-        //this tests for 6 week month
-
-        describe('.getFiveWeek', () => {
-          const month = require('../lib/month.js');
-          it('return 5 week month', () => {
-            expect(month.getFiveWeek(2016, 3)).to.equal(5);
-          });
-          it('return 5 week month', () => {
-            expect(month.getFiveWeek(2016, 4)).to.equal(5);
-          });
-          it('return 5 week month', () => {
-            expect(month.getFiveWeek(2016, 3)).to.equal(4);
-          });
-          it('return 5 week month', () => {
-            expect(month.getFiveWeek(2016, 4)).to.equal(4);
-          });
-        });
-          //this tests for 5 week month
-
-        describe('.getFourWeek', () => {
-          const month = require('../lib/month.js');
-          it('return 4 week month', () => {
-            expect(month.getFourWeek(2015, 2)).to.equal(4);
-          });
-          it('return 4 week month', () => {
-            expect(month.getFourWeek(2009, 2)).to.equal(4);
-          });
-          it('return 4 week month', () => {
-            expect(month.getFourWeek(2015, 2)).to.equal(5);
-          });
-          it('return 4 week month', () => {
-            expect(month.getFourWeek(2015, 2)).to.equal(5);
-          });
-        });
-          //this tests for 4 week month
-
-        describe('.getThirtyDay', () => {
-          const month = require('../lib/month.js');
-          it('return 30 day month', () => {
-            expect(month.getThirtyDay(2016, 4, 30)).to.equal(30);
-          });
-          it('return 30 day month', () => {
-            expect(month.getThirtyDay(2016, 6, 30)).to.equal(30);
-          });
-          it('return 30 day month', () => {
-            expect(month.getThirtyDay(2016, 4, 30)).to.equal(31);
-          });
-          it('return 30 day month', () => {
-            expect(month.getThirtyDay(2016, 6, 30)).to.equal(31);
-          });
-        });
-          //this tests for 30 day month
-
-        describe('.getThirtyOne', () => {
-          const month = require('../lib/month.js');
-          it('return 31 day month', () => {
-            expect(month.getThirtyOne(2016, 7, 31)).to.equal(31);
-          });
-          it('return 31 day month', () => {
-            expect(month.getThirtyOne(2016, 8, 31)).to.equal(31);
-          });
-          it('return 31 day month', () => {
-            expect(month.getThirtyOne(2016, 7, 31)).to.equal(30);
-          });
-          it('return 31 day month', () => {
-            expect(month.getThirtyOne(2016, 8, 31)).to.equal(30);
-          });
-        });
-          //this tests for 31 day month
-
-       var should = require('chai').should(),
+      it('returns 1 (Monday) for March 1, 2100', () => {
+        expect(zellers.modDay(2100, 3, 1)).to.equal(1);
+      });
+      it('returns 0 (Sunday) for March 1, 2200', () => {
+        expect(zellers.modDay(2200, 3, 2)).to.equal(0);
+      });
+      it('returns 4 (Thursday) for March 1, 2300', () => {
+        expect(zellers.modDay(2300, 3, 1)).to.equal(4);
+      });
+    });
+  });
 
 
+  describe('the month output', () => {
+    const month = require('../lib/month.js');
+      it('should handle January', () => {
+        expect(month.setUpMonth(2016, 1)).to.equal(`    January 2016`);
+      });
+  });
+  describe('the weekday output', () => {
+    const month = require('../lib/month.js');
+      it('should print days with spaces', () => {
+        expect(month.setUpWeekDays(2016, 1)).to.equal(`Su Mo Tu We Th Fr Sa\n`);
+      });
+  });
+  describe('the dates with 31 days', () => {
+    const month = require('../lib/month.js');
+     it('should print all the dates in January',() => {
+        expect(month.setUpDates(2016, 1, 1)).to.equal(`                1  2\n 3  4  5  6  7  8  9\n10 11 12 13 14 15 16\n17 18 19 20 21 22 23\n24 25 26 27 28 29 30\n31`);
+      });
+  });
 
-
-
-  });//end of zeller congruence
-});//end of cal
-
-//set up year.js
-//set up arrays for month month.js
-//pull month and year and day to inject into cli
-//need test case for
-//6 week month test done need code
-//5 week month test done need code
-//4 week month test done need code
-//30 day month test done need code
-//31 day month test done need code
-//leap year
-//non leap year
-//integration testing and unit testing(inside the code)
-//1753 minimum year working to 9999
+});
